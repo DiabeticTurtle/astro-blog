f2edd0cdb94d0026db8363f668c198fdd3641490
Bront <diabeticturtle@diabetes.forsale>
2025-06-19 04:38:14 +0000

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "beetus sale";
+export const SITE_DESCRIPTION = "Heres a blog centered around my perspective with, the burdens of disability, and an insight in living with diabetes, and chronic fatigue.";
